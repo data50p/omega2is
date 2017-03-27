a3027b9d6015ebb3d05d5a9d77ac5f686d07f57c
Lars Nordberg <reg50p@comhem.se>
2017-03-27 17:57:14 +0200

    Create story indicator file

--- src/main/java/omega/lesson/repository/Save.java
+++ src/main/java/omega/lesson/repository/Save.java
@@ -3,9 +3,36 @@ package omega.lesson.repository;
 import fpdo.sundry.S;
 import fpdo.xml.Element;
 import fpdo.xml.XML_PW;
+import omega.OmegaContext;
+import omega.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Save {
     public static void save(String fname, Element el) {
+        List stories = el.find("story");
+        if ( stories != null && stories.size() > 0 ) {
+            Element sel = (Element) stories.get(0);
+            String isfirst = sel.findAttr("isfirst");
+            if ( isfirst != null ) {
+                if ( fname.contains("active") ) {
+                    Log.getLogger().fine("ADD story file");
+                    File file = new File(OmegaContext.omegaAssets(fname));
+                    File dir = file.getParentFile().getParentFile();
+                    File storyFile = new File(dir, "story");
+                    if ( ! storyFile.exists() ) {
+                        try {
+                            boolean b = storyFile.createNewFile();
+                            Log.getLogger().info("ADDED story file " + storyFile + ' ' + b);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
         XML_PW xmlpw = new XML_PW(S.createPrintWriterUTF8(fname), false);
         xmlpw.put(el);
         xmlpw.close();
